bd881d64353161dee9161177a55cc08df59aa113
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-02-09 09:58:10 -0700

    finished destructure

--- destructure.js
+++ destructure.js
@@ -1,5 +1,12 @@
 let bigArray = ["apple", 14, 32, 100, { name: "party" }, ["pineapple"]];
 
+let [first, second, third, fourth, fifth, sixth] = bigArray;
+
+console.log(first);
+console.log(third);
+console.log(fifth);
+
+
 // Example:
 // let [firstEl] = bigArray;
 // console.log(firstEl) // prints "apple"
